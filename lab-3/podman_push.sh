7c2bee5300c4111f97d2f131bfc82ca53378ffb6
pie3aq <m.pietrzak.mx@gmail.com>
2024-01-31 19:20:05 +0100

    pierwsze materialy

---
+++ lab-3/podman_push.sh
@@ -0,0 +1,1 @@
+podman push quay.io/pie3aq/debian-mysql:1.0.0
---
+++ lab-3/podman_tag.sh
@@ -0,0 +1,2 @@
+podman tag localhost/debian-mysql:latest quay.io/pie3aq/debian-mysql:latest
+podman tag quay.io/pie3aq/debian-mysql:latest quay.io/pie3aq/debian-mysql:1.0.0
